272d6e08cea2020ea6e80b1821a186bbfe7446c6
Emran <emran53hossain@gmail.com>
2021-08-28 21:58:17 +0600

    Crud

---
+++ CrudApi/Controllers/HomeController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using CrudApi.Models;
+using System.Web.Security;
+
+namespace CrudApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        CRUDEntities db = new CRUDEntities();
+
+        public ActionResult UserRegistration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UserRegistration(Student_Information student_Information)
+        {
+            HttpClient Hc = new HttpClient();
+            Hc.BaseAddress = new Uri("https://localhost:44352/api/StudentInformation");
+            var CreateInfo = Hc.PostAsJsonAsync<Student_Information>("StudentInformation", student_Information);
+            CreateInfo.Wait();
+            var saveData = CreateInfo.Result;
+
+            if (saveData.IsSuccessStatusCode)
+            {
+                return RedirectToAction("StudentInfor");
+            }
+            return View("Create");
+        }
+
+
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(Models.AdminLogin model)
+        {
+            using (var context = new CRUDEntities())
+            {
+                FormsAuthentication.SetAuthCookie(model.UserName, false);
+                bool Isvalid = context.Users.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if (Isvalid)
+                {
+                    return RedirectToAction("StudentInfor");
+                }
+                ModelState.AddModelError("", "Invalid UserName And Password");
+                return View();
+            }
+
+        }
+        public ActionResult StudentInfor()
+        {
+            IEnumerable<Student_Information> student = null;
+            HttpClient Hc = new HttpClient();
+            Hc.BaseAddress = new Uri("https://localhost:44352/api/StudentInformation");
+
+            var consumeapi = Hc.GetAsync("StudentInformation");
+            consumeapi.Wait();
+            var ReadData = consumeapi.Result;
+            if (ReadData.IsSuccessStatusCode)
+            {
+                var display = ReadData.Content.ReadAsAsync<IList<Student_Information>>();
+                display.Wait();
+                student = display.Result;
+            }
+            
+
+            return View(student);
+        }
+        
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(Student_Information student_Information)
+        {
+            HttpClient Hc = new HttpClient();
+            Hc.BaseAddress = new Uri("https://localhost:44352/api/StudentInformation");
+            var CreateInfo = Hc.PostAsJsonAsync<Student_Information>("StudentInformation", student_Information);
+            CreateInfo.Wait();
+            var saveData = CreateInfo.Result;
+
+            if (saveData.IsSuccessStatusCode)
+            {
+                return RedirectToAction("StudentInfor");
+            }
+            return View("Create");
+        }
+        [HttpGet]
+        public ActionResult StudentDetails(int id)
+        {
+            Information obj = null;
+            HttpClient Hc = new HttpClient();
+            Hc.BaseAddress = new Uri("https://localhost:44352/api/StudentInformation");
+            var dc = Hc.GetAsync("StudentInformation?id=" + id.ToString());
+            dc.Wait();
+            var readData = dc.Result;
+            if (readData.IsSuccessStatusCode)
+            {
+                var display = readData.Content.ReadAsAsync<Information>();
+                display.Wait();
+                obj = display.Result;
+            }
+            return View(obj);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Information obj = null;
+            HttpClient Hc = new HttpClient();
+            Hc.BaseAddress = new Uri("https://localhost:44352/api/StudentInformation");
+            var dc = Hc.GetAsync("StudentInformation?id=" + id.ToString());
+            dc.Wait();
+            var readData = dc.Result;
+            if (readData.IsSuccessStatusCode)
+            {
+                var display = readData.Content.ReadAsAsync<Information>();
+                display.Wait();
+                obj = display.Result;
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Edit(Information ec)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:44352/api/StudentInformation");
+            var CreateInfo = hc.PutAsJsonAsync<Information>("StudentInformation", ec);
+            CreateInfo.Wait();
+            var saveData = CreateInfo.Result;
+
+            if (saveData.IsSuccessStatusCode)
+            {
+                return RedirectToAction("StudentInfor");
+            }
+            else
+            {
+                ViewBag.message = "Not Update";
+            }
+            return View(ec);
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:44352/api/StudentInformation");
+            var delrecord=   hc.DeleteAsync("StudentInformation/" +id.ToString());
+            delrecord.Wait();
+            var display = delrecord.Result;
+            if (display.IsSuccessStatusCode)
+            {
+                return RedirectToAction("StudentInfor");
+            }
+            return View("StudentInfor");
+        }
+    }
+}
---
+++ CrudApi/Controllers/StudentInformationController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CrudApi.Models; 
+
+namespace CrudApi.Controllers
+{
+    
+    public class StudentInformationController : ApiController
+    {
+        CRUDEntities db = new CRUDEntities();
+
+        public IHttpActionResult GetStudent()
+        {
+            var result = db.Student_Information.ToList();
+            return Ok(result);
+        }
+        [Authorize]
+        public IHttpActionResult CreateStudent(Student_Information student_Information)
+        {
+            db.Student_Information.Add(student_Information);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        public IHttpActionResult GetDetails(int id)
+        {
+            Information informationDetails = null;
+            informationDetails = db.Student_Information.Where(x => x.ID == id).Select(x => new Information()
+            {
+                ID = x.ID,
+                FastName = x.FastName,
+                LastName = x.LastName,
+                Email = x.Email,
+                Phone = x.Phone,
+                Description = x.Description
+            }).FirstOrDefault<Information>();
+            if(informationDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(informationDetails);
+        }
+
+        public IHttpActionResult Put(Information information)
+        {
+            var Updateinfo = db.Student_Information.Where(x => x.ID == information.ID).FirstOrDefault<Student_Information>();
+            if(Updateinfo!= null)
+            {
+                Updateinfo.ID = information.ID;
+                Updateinfo.FastName = information.FastName;
+                Updateinfo.LastName = information.LastName;
+                Updateinfo.Email = information.Email;
+                Updateinfo.Phone = information.Phone;
+                Updateinfo.Description = information.Description;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var empdel = db.Student_Information.Where(x => x.ID == id).FirstOrDefault();
+            db.Entry(empdel).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
